f68e563df625d2be82f288efd3ca38c15723e8d7
bagassambega <bagassambega@gmail.com>
2024-06-08 18:03:25 +0700

    feat: try to connect database to image processing, create levenshtein distance function for edge case in alay name convert

--- src/database/AlayName.cs
+++ src/database/AlayName.cs
@@ -22,8 +22,52 @@ class AlayName
 
         string pattern = CreatePatternFromValidName(validName);
 
-        return Regex.IsMatch(normalizedAlayName, pattern, RegexOptions.IgnoreCase);
+        bool match = Regex.IsMatch(normalizedAlayName, pattern, RegexOptions.IgnoreCase);
+
+        if (match)
+        {
+            return true;
+        } else
+        {
+            if (LevenshteinDistance(normalizedAlayName, validName) <= 5)
+            {
+                return true;
+            }
+            return false;
+        }
     }
+
+    public static int LevenshteinDistance(string source, string target)
+    {
+        if (string.IsNullOrEmpty(source))
+            return string.IsNullOrEmpty(target) ? 0 : target.Length;
+
+        if (string.IsNullOrEmpty(target))
+            return source.Length;
+
+        int sourceLength = source.Length;
+        int targetLength = target.Length;
+
+        int[,] distance = new int[sourceLength + 1, targetLength + 1];
+
+        for (int i = 0; i <= sourceLength; distance[i, 0] = i++) { }
+        for (int j = 0; j <= targetLength; distance[0, j] = j++) { }
+
+        for (int i = 1; i <= sourceLength; i++)
+        {
+            for (int j = 1; j <= targetLength; j++)
+            {
+                int cost = (target[j - 1] == source[i - 1]) ? 0 : 1;
+
+                distance[i, j] = Math.Min(
+                    Math.Min(distance[i - 1, j] + 1, distance[i, j - 1] + 1),
+                    distance[i - 1, j - 1] + cost);
+            }
+        }
+
+        return distance[sourceLength, targetLength];
+    }
+
 
     public static string NormalizeAlayName(string alayName)
     {
@@ -38,6 +82,11 @@ class AlayName
         return new string(normalizedArray);
     }
 
+    /// <summary>
+    /// Create a pattern from the valid name, will be used to match with the alayName
+    /// </summary>
+    /// <param name="validName">The valid name</param>
+    /// <returns>Pattern generated from the valid name (create all lowercase and delete all vocals)</returns>
     public static string CreatePatternFromValidName(string validName)
     {
         string pattern = Regex.Replace(validName, "[aeiouAEIOU]", "[aeiouAEIOU]?");
